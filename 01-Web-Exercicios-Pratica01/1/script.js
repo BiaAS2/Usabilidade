e721b0f2038ab9e22f8e91c010c575327c8bee83
LucelhoCristiano <lucelhocristiano28@gmail.com>
2023-09-15 18:10:35 -0300

    Atualizando conteudo da Pagina
    Co-authored-by: ♡Bia♡ <BiaAS2@users.noreply.github.com>
    Co-authored-by: machidaftw <machidaftw@users.noreply.github.com>

--- 01-Web-Exercicios-Pratica01/1/script.js
+++ 01-Web-Exercicios-Pratica01/1/script.js
@@ -3,16 +3,19 @@ const close = document.getElementById("close");
 const menu = document.querySelector(".menu");
 const content = document.querySelector(".content");
 const navigation = document.querySelector("nav");
+const page = document.documentElement;
 
 function abrirMenu() {
   menu.classList.add("showNav");
   content.classList.add("showNav");
+  page.classList.add('noOverflow')
   navigation.style.display = "block";
 }
 
 function fecharMenu() {
   menu.classList.remove("showNav");
   content.classList.remove("showNav");
+  page.classList.remove('noOverflow')
   navigation.style.display = "none";
 }
 
