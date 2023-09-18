f3d06c6f61127ab953b9df02d2dafd7b425e2787
machidaftw <machidaftw1@hotmail.com>
2023-09-18 16:30:30 -0300

    Otimizando codigo

--- Web-Exercicios-Pratica 2/Atividade 1/script.js
+++ Web-Exercicios-Pratica 2/Atividade 1/script.js
@@ -2,5 +2,7 @@ function calcularIdade() {
   var anoNascimento = document.getElementById("ano-nascimento").value
   var anoAtual = document.getElementById("ano-atual").value
   var idade = anoAtual - anoNascimento
-  document.getElementById("resultado").innerHTML = "Idade: " + idade + " anos"
+  document.getElementById("resultado").innerHTML = "Idade: " + idade + " anos";
+
+  
  }
