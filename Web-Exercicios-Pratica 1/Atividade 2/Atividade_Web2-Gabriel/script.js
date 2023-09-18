65be0b9ba47911687e10b40dc57effc07d3ef71f
Gabriel <112487706+GabrielAnjosBento@users.noreply.github.com>
2023-09-18 01:27:57 -0300

    Update script.js

---
+++ Web-Exercicios-Pratica 1/Atividade 2/Atividade_Web2-Gabriel/script.js
@@ -0,0 +1,13 @@
+var texto = document.getElementById('base')
+
+function texto1(){
+texto.innerText = 'Desenho: O desenho é uma habilidade fundamental nas artes visuais e no design. Os cursos de desenho ajudam os alunos a desenvolver suas habilidades de observação, representação e expressão visual. Isso inclui desenho de figura humana, natureza morta, paisagens e outros temas. O desenho é uma base importante para muitas disciplinas artísticas.'
+}
+
+function texto2(){
+texto.innerText = 'História da Arte e Teoria: Os estudantes de Arte e Design geralmente estudam a história da arte, explorando movimentos artísticos, artistas famosos e contextos culturais. Além disso, eles aprendem sobre teorias e críticas de arte, o que os ajuda a compreender o significado e o contexto por trás das obras de arte.'
+}
+
+function texto3(){
+texto.innerText = 'Design Visual: No campo do design, os cursos de design visual são cruciais. Eles cobrem princípios de design, tipografia, cores, layout, design de logotipos, design de embalagens e muito mais. Esses cursos preparam os alunos para trabalhar como designers gráficos, de produtos, de moda, entre outros.'
+}
