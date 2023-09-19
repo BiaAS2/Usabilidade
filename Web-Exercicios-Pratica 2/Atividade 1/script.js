849370cfcd6f446cdaef3b98101e5436fe40c213
machidaftw <machidaftw1@hotmail.com>
2023-09-18 21:16:46 -0300

    modificando arquivos da segunda atividade

--- Web-Exercicios-Pratica 2/Atividade 1/script.js
+++ Web-Exercicios-Pratica 2/Atividade 1/script.js
@@ -1,8 +1,9 @@
 function calcularIdade() {
-  var anoNascimento = document.getElementById("ano-nascimento").value
-  var anoAtual = document.getElementById("ano-atual").value
-  var idade = anoAtual - anoNascimento
-  document.getElementById("resultado").innerHTML = "Idade: " + idade + " anos";
+  var anoNascimento = document.getElementById("ano-nascimento").value;
+  var anoAtual = document.getElementById("ano-atual").value;
+  var idade = anoAtual - anoNascimento;
+  let ress = document.getElementById("resultado");
 
-  
- }
+  ress.innerHTML = "Idade: " + idade + " anos";
+  ress.style.padding = "1rem";
+}
--- Web-Exercicios-Pratica 2/Atividade 3/script3.js
+++ Web-Exercicios-Pratica 2/Atividade 3/script3.js
@@ -2,9 +2,11 @@ function calcular() {
   var tempC = Number(document.getElementById("grausC").value);
   var resultado = document.getElementById("result");
 
-  var grau2 = tempC * 2 + 32;
-
-  alert(tempC + " em graus Celsius é igual a " + grau2 + " em Farenheit");
-  resultado.innerText = grau2 + "°F";
-  resultado.style.backgroundColor = "red";
+  var grau2 = tempC * 1.8 + 32;
+  {
+    resultado.innerText = grau2.toFixed(2) + "°F";
+    resultado.style.backgroundColor = "green";
+    resultado.style.borderRadius = ".5rem";
+    resultado.style.padding = ".5rem";
+  }
 }
