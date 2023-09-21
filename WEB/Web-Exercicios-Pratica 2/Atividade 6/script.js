1d830b0d079315f3ff1a9e47697eeb73a548a9b6
♡Bia♡ <112487610+BiaAS2@users.noreply.github.com>
2023-09-20 22:49:19 -0300

    atividade 6 pronta

--- WEB/Web-Exercicios-Pratica 2/Atividade 6/script.js
+++ WEB/Web-Exercicios-Pratica 2/Atividade 6/script.js
@@ -1,16 +1,24 @@
 function mostrarCalculos() {
   var R = document.getElementById('raio').value
 
+  //Verifica se a raíz é menor ou igual a 0(zero)
+  if (R <= 0) {
+    alert('Número Inexistente! \nO valor que você está procurando não existe. \nTente novamente.')
+  }
+  else {
+    
   // Calcula o comprimento da esfera (circunferência)
   const C = (2 * Math.PI * R).toFixed(2)
-  document.getElementById('comprimento').innerHTML = 'Comprimento da Esfera:' + C
+  document.getElementById('comprimento').innerHTML =  C
 
   // Calcula a área da esfera
   const A = (Math.PI * Math.pow(R, 2)).toFixed(2)
-  document.getElementById('area').innerHTML = 'Área da Esfera:' + A
+  document.getElementById('area').innerHTML =  A
 
   // Calcula o volume da esfera
   const V = ((3 / 4) * Math.PI * Math.pow(R, 3)).toFixed(2)
-  document.getElementById('volume').innerHTML = 'Volume da Esfera:' + V
+  document.getElementById('volume').innerHTML =  V
+  }
+
 }
 
