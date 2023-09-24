415156cb153b851157c3a14c434ad447807c0fcf
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2023-09-24 11:10:35 -0300

    Add: prática 2 - atividade 2 ! Co-authored-by: machidaftw <machidaftw1@hotmail.com>

---
+++ MOBILE/Pratica_2/atividade_2/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.home),
+        title: const Text('Página Inicial'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: const [
+            //1ª imagem
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Image(
+                  image: NetworkImage(
+                    'https://picsum.photos/250?image=10', 
+                  ),
+                ),
+              ),
+            ),
+
+            //2ª imagem
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Image(
+                  image: NetworkImage(
+                    'https://picsum.photos/250?image=25', 
+                  ),
+                ),
+              ),
+            ),
+
+            //3ª imagem
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Image(
+                  image: NetworkImage(
+                    'https://picsum.photos/250?image=15', 
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Exemplo de botão',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
