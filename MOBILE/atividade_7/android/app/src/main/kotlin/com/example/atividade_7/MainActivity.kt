5f4dde64419dc2fabb65b793ef8d0edd55683297
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2023-09-24 09:34:49 -0300

    Add: atividade 7

---
+++ MOBILE/atividade_7/android/app/src/main/kotlin/com/example/atividade_7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade_7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ MOBILE/atividade_7/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.menu),
+        title: const Text('Primeiro Site'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              "Beatriz Alves;\n"
+              'Lucelho Cristiano;\n'
+              'Pedro Henrique;\n'
+              'Bianca Mayra;\n'
+              'João Vitor;\n'
+              'Gabriel Anjos;\n'
+              'Rafael Prazeres.',
+              style: TextStyle(
+                color: Colors.yellow,
+                fontSize: 40,
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.blue[900],
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Modo Escuro',
+        child: Icon(Icons.brightness_2),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+    );
+  }
+}
