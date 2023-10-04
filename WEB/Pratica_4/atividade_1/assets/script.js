5f898a6d3cc45d663dd810a7309d461f0f21b79c
♡Bia♡ <112487610+BiaAS2@users.noreply.github.com>
2023-10-03 22:41:40 -0300

    fix: tentando mexer no script da prática 4, ainda não ta pronto

--- WEB/Pratica_4/atividade_1/assets/script.js
+++ WEB/Pratica_4/atividade_1/assets/script.js
@@ -6,4 +6,20 @@ function calcularNotas(cell) {
 
   var total = p1 + p2 + t1
   row.cells[4].textContent = total.toFixed(2) // Exibe a média com 2 casas decimais
+
+  var tabela = document.getElementById("tabela").getElementsByTagName("tbody")[0]
+  var linhas = tabela.getElementsByTagName(".notaTotal")
+
+  // Loop pelas linhas da tabela
+  for (var i = 0; i < linhas.length; i++) {
+    var celulaNota = linhas[i].getElementsByTagName("td")[1]
+    var nota = parseFloat(celulaNota.textContent)
+    if (!isNaN(nota)) {
+      total += nota
+    }
+  }
+
+  // Calcula a média e atualiza a célula de média
+  var media = total / tabela.rows.length
+  document.getElementById("mediaTurma").textContent = media.toFixed(2) // Limita a 2 casas decimais
 }
