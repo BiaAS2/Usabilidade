4304937cc9d4ae2925e8dd965a5e456dacb460fe
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2023-10-12 12:52:22 -0300

    :tada: ADD: Pratica_6

---
+++ MOBILE/Pratica_6/atividade_4/android/app/src/main/kotlin/com/example/atividade_4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ MOBILE/Pratica_6/atividade_5/android/app/src/main/kotlin/com/example/atividade_5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade_5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
