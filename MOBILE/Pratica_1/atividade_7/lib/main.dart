a2d7be5ca324a03d0955d78b8b1310cc410e2bfd
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2023-10-11 21:55:13 -0300

    :tada: Refactor: Pratica_1 : Atividade_6, 7 e 8

--- MOBILE/Pratica_1/atividade_7/lib/main.dart
+++ MOBILE/Pratica_1/atividade_7/lib/main.dart
@@ -17,49 +17,24 @@ class Home extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Adicionando Container envolvendo o Text
-            Container(
-              padding: EdgeInsets.all(16), // Adicionando um espaçamento interno
-              decoration: BoxDecoration(
-                color:
-                    Color.fromARGB(115, 180, 180, 180), // Cor do fundo da caixa
-                borderRadius: BorderRadius.circular(8), // Borda arredondada
-              ),
-              child: const Column(
-                children: [
-                  Text(
-                    'Lucelho Cristiano Vieira Da Silva',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 251, 0), // Amarelo
-                      fontSize: 16,
-                    ),
-                  ),
-                  SizedBox(height: 16), // Adicionando um espaço entre os textos
-                  Text(
-                    '"Mas pra quem tem pensamento forte, o impossível e só questão de opinião."\n'
-                    '\n'
-                    '"A coragem não é a ausência do medo, mas a capacidade de enfrentá-lo."\n'
-                    '\n'
-                    'Lei de Leblanc "Mais tarde é igual a nunca."\n'
-                    '\n'
-                    '"A vida é feita de escolhas. Quando você dá um passo à frente, inevitavelmente algo fica para trás."\n'
-                    '\n'
-                    '"A maior glória em viver não está em nunca cair, mas em levantar cada vez que caímos."\n'
-                    '\n'
-                    '"Acredite em si próprio e chegará um dia em que os outros não terão outra escolha senão acreditar com você."',
-                    style: TextStyle(
-                      color: Colors.white, // Branco
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
+          children: const [
+            Text(
+              "Beatriz Alves;\n"
+              'Lucelho Cristiano;\n'
+              'Pedro Henrique;\n'
+              'Bianca Mayra;\n'
+              'João Vitor;\n'
+              'Gabriel Anjos;\n'
+              'Rafael Prazeres.',
+              style: TextStyle(
+                color: Colors.yellow,
+                fontSize: 40,
               ),
             ),
           ],
         ),
       ),
-      backgroundColor: Color.fromARGB(184, 44, 44, 44),
+      backgroundColor: Colors.blue[900],
       floatingActionButton: const FloatingActionButton(
         onPressed: null,
         tooltip: 'Modo Escuro',
--- MOBILE/Pratica_1/atividade_8/lib/main.dart
+++ MOBILE/Pratica_1/atividade_8/lib/main.dart
@@ -12,24 +12,23 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         leading: const Icon(Icons.search),
         title: const Text('Pesquisar Imagem'),
-        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        backgroundColor: Color.fromARGB(255, 71, 40, 184),
       ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.network(
-              'https://media.giphy.com/media/pt0EKLDJmVvlS/giphy.gif',
-            ),
+                'https://media.giphy.com/media/pt0EKLDJmVvlS/giphy.gif'),
           ],
         ),
       ),
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      backgroundColor: Color.fromARGB(255, 191, 152, 255),
       floatingActionButton: const FloatingActionButton(
         onPressed: null,
         tooltip: 'Baixar Imagem',
         child: Icon(Icons.file_download),
-        backgroundColor: Color.fromARGB(255, 0, 17, 255),
+        backgroundColor: Color.fromARGB(255, 38, 43, 114),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
