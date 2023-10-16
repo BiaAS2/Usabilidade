63be3fadeb6103cbc494c737e0eae0e650478507
Lucelho Cristiano Vieira Silva <lucelhocristiano28@gmail.com>
2023-10-16 17:41:31 -0300

    Add: pratica_5 - atividade_2

--- MOBILE/Pratica_5/atividade_2/lib/main.dart
+++ MOBILE/Pratica_5/atividade_2/lib/main.dart
@@ -1,125 +1,136 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+      MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (_) => PrimeiraTela(), // _ = context.
+          '/segunda': (_) => SegundaTela(), // _ = context.
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+          '/terceira': (_) => TerceiraTela(), // _ = context.
+          '/quarta': (_) => QuartaTela(),
+        },
+        debugShowCheckedModeBanner: false,
+      ),
+    );
 
-  // This widget is the root of your application.
+class PrimeiraTela extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    Corpo corpo = const Corpo('1');
+    Botao botao = const Botao('segunda');
+    return Tela('Primeira Tela', corpo, botao);
+  }
+}
+
+class SegundaTela extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Corpo corpo = const Corpo('2');
+    Botoes botoes = const Botoes('terceira');
+    return Tela('Segunda Tela', corpo, botoes);
+  }
+}
+
+class TerceiraTela extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Corpo corpo = const Corpo('3');
+    Botoes botoes = const Botoes('quarta');
+    return Tela('Terceira Tela', corpo, botoes);
+  }
+}
+
+class QuartaTela extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Corpo corpo = const Corpo('4');
+    Botoes botoes = const Botoes('');
+    return Tela('Quarta Tela', corpo, botoes);
+  }
+}
+
+class Corpo extends StatelessWidget {
+  final String texto;
+  const Corpo(this.texto);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(40),
+      margin: const EdgeInsets.all(25),
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      child: Text(
+        texto,
+        style: const TextStyle(
+          fontSize: 45,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Botao extends StatelessWidget {
+  final String proxima;
+  const Botao(this.proxima);
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return ElevatedButton(
+      child: const Icon(Icons.navigate_next),
+      onPressed: () {
+        Navigator.pushNamed(context, '/$proxima');
+      },
+    );
+  }
+}
+
+class Botoes extends StatelessWidget {
+  final String proxima;
+  const Botoes(this.proxima);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        ElevatedButton(
+          child: const Icon(Icons.navigate_before),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        ElevatedButton(
+          child: const Icon(Icons.navigate_next),
+          onPressed: () {
+            Navigator.pushNamed(context, '/$proxima');
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class Tela extends StatelessWidget {
+  final String titulo;
+  final Widget corpo, navegacao;
+  const Tela(this.titulo, this.corpo, this.navegacao);
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(titulo),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            corpo,
+            navegacao,
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
