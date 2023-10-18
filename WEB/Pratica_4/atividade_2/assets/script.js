04e282c5f015e132b7c8b1d856305bc1bb1cb9cd
Pedro Frois <pedrohfrois4@gmail.com>
2023-10-18 01:35:35 -0300

    Criando readme.md

--- WEB/Pratica_4/atividade_2/assets/script.js
+++ WEB/Pratica_4/atividade_2/assets/script.js
@@ -1,55 +1,58 @@
 function calcularNotas(cell) {
-  var row = cell.parentElement;
-  var p1 = parseFloat(row.cells[1].textContent) || 0;
-  var p2 = parseFloat(row.cells[2].textContent) || 0;
-  var t1 = parseFloat(row.cells[3].textContent) || 0;
+  var row = cell.parentElement
+  var p1 = parseFloat(row.cells[1].textContent) || 0
+  var p2 = parseFloat(row.cells[2].textContent) || 0
 
-  var total = p1 + p2 + t1;
-  row.cells[4].textContent = total.toFixed(1); // Exibe a média com 2 casas decimais
+  var total = p1 + p2
+  row.cells[3].textContent = total.toFixed(1) // Exibe a média com 2 casas decimais
 
   // Chamar a função inicialmente
-  calcularMedia();
+  calcularMediaWeb()
 }
 
-function calcularMedia() {
-  const tabela = document.getElementById("tabela");
-  const linhas = tabela.querySelectorAll("tbody tr");
-  let soma = 0;
-  let count = 0;
+function calcularNotasLogica(cell) {
+  var row = cell.parentElement
+  var p1 = parseFloat(row.cells[1].textContent) || 0
+  var p2 = parseFloat(row.cells[2].textContent) || 0
 
-  linhas.forEach((linha) => {
-    const colunas = linha.querySelectorAll("td");
-    for (let i = 4; i < colunas.length - 1; i++) {
-      const valor = parseFloat(colunas[i].textContent);
-      if (!isNaN(valor)) {
-        soma += valor;
-        count++;
-      }
+  var total = p1 + p2
+  row.cells[3].textContent = total.toFixed(1) // Exibe a média com 1 casa decimal
+
+  // Chamar a função de cálculo de média para Lógica de Programação
+  calcularMediaLogica()
+}
+
+function calcularMediaWeb() {
+  let notasWeb = document.querySelectorAll("#tabela tbody:nth-of-type(1) td.notaTotal");
+  let somaWeb = 0;
+  let countWeb = 0;
+
+  for (let i = 0; i < notasWeb.length; i++) {
+    let notaWeb = parseFloat(notasWeb[i].innerText);
+    if (!isNaN(notaWeb)) {
+      somaWeb += notaWeb;
+      countWeb++;
     }
-  });
-
-  const media = count > 0 ? soma / count : 0;
-  document.getElementById("mediaTurma").textContent = media.toFixed(1);
-
-  // Adicionar um ouvinte de evento para chamar a função sempre que houver uma mudança na tabela
-  document.getElementById("tabela").addEventListener("input", calcularMedia);
+  }
+  let mediaWeb = somaWeb / countWeb;
+  document.getElementById("mediaWeb").innerText = mediaWeb.toFixed(2);
 }
-function mostrarCelula() {
-  const btn = document.querySelector(".btn");
-  const tabela = document.querySelector("#tabela");
 
-  btn.innerHTML = "Clique e veja a tabela";
-  
-  btn.addEventListener("click", () => {
-    tabela.classList.toggle("visible");
-    btn.classList.toggle("right");
+function calcularMediaLogica() {
+  let notasLogica = document.querySelectorAll("#tabela tbody:nth-of-type(1) td.notaTotal2");
+  let somaLogica = 0;
+  let countLogica = 0;
 
-    if (btn.classList.contains("right")) {
-      btn.innerHTML = "Clique e esconda tabela";
-    } else {
-      btn.innerHTML = "Clique e veja a tabela";
+  for (let i = 0; i < notasLogica.length; i++) {
+    let notaLogica = parseFloat(notasLogica[i].innerText);
+    if (!isNaN(notaLogica)) {
+      somaLogica += notaLogica;
+      countLogica++;
     }
-  });
+  }
+  let mediaLogica = somaLogica / countLogica;
+  document.getElementById("mediaProgram").innerText = mediaLogica.toFixed(2);
 }
 
-mostrarCelula();
+// Adiciona um ouvinte de evento para chamar a função sempre que houver uma mudança na tabela
+document.getElementById("tabela").addEventListener("input");
