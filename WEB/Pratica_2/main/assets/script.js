341b82e88d9e9381cef9663b3c4e92a458ea0558
machidaftw <89604440+machidaftw@users.noreply.github.com>
2023-10-18 09:40:28 -0300

    Atualizando surpresa

--- WEB/Pratica_2/main/assets/script.js
+++ WEB/Pratica_2/main/assets/script.js
@@ -3,9 +3,10 @@ const surp = document.querySelector(".surprise");
 const music = document.querySelector("audio");
 
 btn.addEventListener("click", () => {
-  surp.classList.add("bruh");
+  
 btn.classList.add('nobtn')
   if (music.paused) {
+    surp.classList.add("bruh");
     music.play();
   } else {
     music.pause();
