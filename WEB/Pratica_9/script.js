fe81f3e6f11a18342ac44856dde2626621bd9439
Gabriel <gabrielanjosbento@gmail.com>
2023-11-01 17:21:36 -0300

    Pratica 9-1 (CSS)

--- WEB/Pratica_9/script.js
+++ WEB/Pratica_9/script.js
@@ -1,16 +1,16 @@
-function imprimir(){
-/*Possua uma função que receba o número natural informado como argumento 
-e imprima na tela todos os números naturais ímpares menores ou iguais ao número recebido.*/
+function imprimir() {
+    /*Possua uma função que receba o número natural informado como argumento 
+    e imprima na tela todos os números naturais ímpares menores ou iguais ao número recebido.*/
 
-var numero = parseInt(document.getElementById("numero").value);
-var result = document.getElementById("resultado");
+    var numero = parseInt(document.getElementById("numero").value);
+    var result = document.getElementById("resultado");
 
-resultado.textContent = "Numeros naturais ímpares menores ou iguais a " + numero + " :";
-for (i = 0; i <= numero ; i++){
-    if (i % 2 !== 0) {
-        resultado.textContent += i + " , ";
+    resultado.textContent = "Numeros naturais ímpares menores ou iguais a " + numero + " : ";
+    for (i = 0; i <= numero; i++) {
+        if (i % 2 !== 0) {
+            resultado.textContent += i + " ";
+        }
+
     }
-
-}
 }
 
