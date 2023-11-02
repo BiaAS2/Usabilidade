0c3d5fc282d3e30d9aefa31db9c573d42cd9a522
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2023-11-02 15:27:24 -0300

    fix: Corrigido Nan adicionado a tabela

--- WEB/Pratica_9/script.js
+++ WEB/Pratica_9/script.js
@@ -1,16 +1,18 @@
 function imprimir() {
-    /*Possua uma função que receba o número natural informado como argumento 
-    e imprima na tela todos os números naturais ímpares menores ou iguais ao número recebido.*/
-
     var numero = parseInt(document.getElementById("numero").value);
     var result = document.getElementById("resultado");
 
-    resultado.textContent = "Numeros naturais ímpares menores ou iguais a " + numero + " : ";
+    
+    if (isNaN(numero)) {
+        alert("Nenhum valor foi inserido!");
+        result.textContent = "";
+        return;
+    }
+
+    result.textContent = "Numeros naturais ímpares menores ou iguais a " + numero + " : ";
     for (i = 0; i <= numero; i++) {
         if (i % 2 !== 0) {
-            resultado.textContent += i + " ";
+            result.textContent += i + " ";
         }
-
     }
 }
-
