e0e578a5401127f80572e98d856e8a06bff860f4
Gabriel <gabrielanjosbento@gmail.com>
2023-11-15 19:42:13 -0300

    Iniciando Atividade 2 da Pratica 9

---
+++ WEB/Pratica_9/atividade_1/script2.js
@@ -0,0 +1,30 @@
+var vetor = [];
+
+var valor1 = ["Numero 1"],
+    valor2 = ["Numero 2"],
+    valor3 = ["Numero 3"],
+    valor4 = ["Numero 4"],
+    valor5 = ["Numero 5"],
+    valor6 = ["Numero 6"],
+    valor7 = ["Numero 7"],
+    valor8 = ["Numero 8"],
+    valor9 = ["Numero 9"],
+    valor10 = ["Numero 10"];
+    
+    vetor.push(valor1);
+    vetor.push(valor2);
+    vetor.push(valor3);
+    vetor.push(valor4);
+    vetor.push(valor5);
+    vetor.push(valor6);
+    vetor.push(valor7);
+    vetor.push(valor8);
+    vetor.push(valor9);
+    vetor.push(valor10);
+
+function ler(){
+    var valor = parseInt(document.getElementById("valor").value)
+    var lista = document.getElementById("listar")
+
+    
+}
