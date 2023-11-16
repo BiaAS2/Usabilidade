285956d7da28ce087340b14f85faddd404b53123
machidaftw <machidaftw1@hotmail.com>
2023-11-16 15:14:18 -0300

    Iniciando javascript da calculadora

--- WEB/Pratica_10/atividade-1/script.js
+++ WEB/Pratica_10/atividade-1/script.js
@@ -24,11 +24,26 @@ function criarElemento() {
     ".",
   ];
 
+  let soma = 0;
+
   // Loop para criar e adicionar 18 elementos li
   for (let i = 0; i < 18; i++) {
     let liElement = document.createElement("li");
     liElement.textContent = rotulosBotoes[i]; // Usa o índice i para acessar o rótulo correspondente
     ulElement.appendChild(liElement);
+
+    // Adiciona um evento de clique ao elemento li
+    liElement.addEventListener("click", () => {
+      // Seleciona a div de resultado
+      let divResultado = document.querySelector(".resultado");
+
+      divResultado.textContent += liElement.textContent;
+
+      if (liElement.textContent == "C") {
+        divResultado.textContent = "";
+      }
+      
+    });
   }
 }
 
