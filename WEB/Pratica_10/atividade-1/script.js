0e309e368b02bc7177a75c41110932b65c23c4a5
♡Bia♡ <112487610+BiaAS2@users.noreply.github.com>
2023-11-28 01:41:50 -0300

    Adicionando layout na atividade 10

--- WEB/Pratica_10/atividade-1/script.js
+++ WEB/Pratica_10/atividade-1/script.js
@@ -1,124 +1,104 @@
-// Seleciona o elemento ul
-function criarElemento() {
-  const ulElement = document.querySelector("ul");
+let currentInput = '';
+let fullExpression = '';
+let operator = '';
+let result = 0;
 
-  // Define os rótulos para os botões
-  const rotulosBotoes = [
-    "C",
-    "+-",
-    "%",
-    "/",
-    "7",
-    "8",
-    "9",
-    "*",
-    "4",
-    "5",
-    "6",
-    "-",
-    "1",
-    "2",
-    "3",
-    "+",
-    "0", 
-    ".",
-  ];
-
-  let soma = 0;
-
-  // Loop para criar e adicionar 18 elementos li
-  for (let i = 0; i < 18; i++) {
-    let liElement = document.createElement("li");
-    liElement.textContent = rotulosBotoes[i]; // Usa o índice i para acessar o rótulo correspondente
-    ulElement.appendChild(liElement);
-
-    // Adiciona um evento de clique ao elemento li
-    liElement.addEventListener("click", () => {
-      // Seleciona a div de resultado
-      let divResultado = document.querySelector(".resultado");
-
-      divResultado.textContent += liElement.textContent;
-
-      if (liElement.textContent == "C") {
-        divResultado.textContent = "";
-      }
-    });
-  }
+function appendNumber(number) {
+    currentInput += number;
+    fullExpression += number;
+    updateDisplay();
 }
-criarElemento();
 
-// Variáveis para armazenar a entrada e a operação
-let entrada = "";
-let a = "";
-let operacao = "";
+function setOperator(op) {
+    operator = op;
+    result = Number(currentInput);
+    fullExpression += operator;
+    currentInput = '';
+    updateDisplay();
+}
 
-// Adiciona um evento de clique a cada botão
-let botoes = document.querySelectorAll("li");
-botoes.forEach((botao) => {
-  botao.addEventListener("click", () => {
-    // Se o botão é um sinal aritmético, define a e operacao
-    if (["+", "-", "*", "/"].includes(botao.textContent)) {
-      a = parseFloat(entrada);
-      operacao = botao.textContent;
-      entrada = "";
-    } else {
-      // Se o botão é um número, adiciona à entrada
-      entrada += botao.textContent;
+function clearDisplay() {
+    currentInput = '';
+    operator = '';
+    result = 0;
+    fullExpression = '';
+    updateDisplay();
+}
+
+function calculateResult() {
+    // Divide a expressão completa em partes usando operadores
+    let parts = fullExpression.split(/([+x/-])/);
+
+    // Realiza cálculos de multiplicação e divisão primeiro
+    for (let i = 1; i < parts.length; i += 2) {
+        let operator = parts[i];
+        if (operator === 'x' || operator === '/') {
+            let num1 = Number(parts[i - 1]);
+            let num2 = Number(parts[i + 1]);
+
+            if (operator === 'x') {
+                parts.splice(i - 1, 3, (num1 * num2).toString());
+            } else {
+                if (num2 !== 0) {
+                    parts.splice(i - 1, 3, (num1 / num2).toString());
+                } else {
+                    // Lida com divisão por zero
+                    clearDisplay();
+                    alert("Erro: Divisão por zero.");
+                    return;
+                }
+            }
+
+            i -= 2; // Volta dois passos para verificar a próxima operação
+        }
     }
-  });
-});
 
-// Adiciona um evento de clique ao botão de igual
-document.querySelector(".calcular").addEventListener("click", () => {
-  // Obtém o valor de b
-  let b = parseFloat(entrada);
+    // Realiza cálculos de adição e subtração
+    let result = Number(parts[0]);
+    for (let i = 1; i < parts.length; i += 2) {
+        let operator = parts[i];
+        let num = Number(parts[i + 1]);
 
-  // Calcula o resultado e exibe na div de resultado
-  let resultado = calcular(operacao, a, b);
-  document.querySelector(".resultado").textContent = resultado;
+        if (operator === '+') {
+            result += num;
+        } else if (operator === '-') {
+            result -= num;
+        }
+    }
 
-  // Limpa a entrada e a operação
-  entrada = "";
-  operacao = "";
+    // Arredonda o resultado para uma casa decimal se necessário
+    result = Math.round(result * 10) / 10;
 
-  // quando o botão de igual for clicado a pagina ira recarregar depois de 5 segundos
-  setTimeout(function () {
-    location.reload();
-  }, 2500);
-});
-
-function somar(a, b) {
-  return Number(a + b);
+    fullExpression = result.toString();
+    currentInput = '';
+    operator = '';
+    updateDisplay();
 }
 
-function subtrair(a, b) {
-  return Number(a - b);
+
+function updateDisplay() {
+    document.getElementById('display').value = fullExpression;
 }
 
-function multiplicar(a, b) {
-  return Number(a * b);
+function appendDecimal() {
+    // Verifica se já existe um ponto na entrada
+    if (!currentInput.includes('.')) {
+        currentInput += '.';
+        fullExpression += '.';
+        updateDisplay();
+    }
 }
 
-function dividir(a, b) {
-  if (b !== 0) {
-    return Number(a / b);
-  } else {
-    return "Erro: Divisão por zero";
-  }
+function appendPercentage() {
+    // Divide o número atual por 100
+    currentInput = (Number(currentInput) / 100).toString();
+    fullExpression = currentInput;
+    updateDisplay();
 }
 
-// Função para gerenciar a entrada do usuário e chamar a função de operação apropriada
-function calcular(operacao, a, b) {
-  switch (operacao) {
-    case "+":
-      return somar(a, b);
-    case "-":
-      return subtrair(a, b);
-    case "*":
-      return multiplicar(a, b);
-    case "/":
-      return dividir(a, b);
-    default:
-      return "Erro: Operação inválida";
-  }
+function toggleSign() {
+    // Inverte o sinal do número atual
+    currentInput = (Number(currentInput) * -1).toString();
+    fullExpression = currentInput;
+    updateDisplay();
 }
